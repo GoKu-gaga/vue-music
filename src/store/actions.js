bb4e49d40a5c14f12dd4241c7b091b4d40a87a4f
yuanyican <max.yuan.ivan@gmail.com>
2018-06-11 23:41:27 +0800

    update suggest

--- src/store/actions.js
+++ src/store/actions.js
@@ -31,3 +31,44 @@ export const randomPlay = function({commit}, {list}) {
   commit(types.SET_FULL_SCREEN, true)
   commit(types.SET_PLAYING_STATE, true)
 }
+
+export const insertSong = function({commit, state}, song) {
+  let playList = state.playlist.slice()
+  let sequenceList = state.sequenceList.slice()
+  let currentIndex = state.currentIndex
+  // 记录当前歌曲
+  let currentSong = playList[currentIndex]
+  // 查找列表中是否有待插入的歌曲并返回索引
+  let fpIndex = findIndex(playList, song)
+  // 因为是插入歌曲，所以索引+1
+  currentIndex++
+  // 插入这首歌到当前位置
+  playList.splice(currentIndex, 0, song)
+  if (fpIndex > -1) {
+    // 如果当前插入序号大于列表中的序号
+    if (currentIndex > fpIndex) {
+      playList.splice(fpIndex, 1)
+      currentIndex--
+    } else {
+      playList.splice(fpIndex + 1, 1)
+    }
+  }
+  let currentSIndex = findIndex(sequenceList, currentSong) + 1
+  let fsIndex = findIndex(sequenceList, song)
+
+  sequenceList.splice(currentSIndex, 0, song)
+
+  if (fsIndex > -1) {
+    if (currentSIndex > fsIndex) {
+      sequenceList.splice(fsIndex, 1)
+    } else {
+      sequenceList.splice(fsIndex + 1, 1)
+    }
+  }
+
+  commit(types.SET_PLAYLIST, playList)
+  commit(types.SET_SEQUENCE_LIST, sequenceList)
+  commit(types.SET_CURRENT_INDEX, currentIndex)
+  commit(types.SET_FULL_SCREEN, true)
+  commit(types.SET_PLAYING_STATE, true)
+}
